db62ba15eb0fb875346eec64c9f475bee18e9b8f
obsfx <balandiomer@gmail.com>
2020-06-01 02:42:48 +0300

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -13,12 +13,6 @@ sudo pip3 install pynvim
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-NVMCONFIG='export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' 
-
-echo NVMCONFIG >> ~/.bashrc 
-echo NVMCONFIG >> ~/.zshrc
-
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh) $(whoami)
 
