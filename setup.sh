9d5a72b3426c6cea65c6f585b4d021a5494764f1
obsfx <balandiomer@gmail.com>
2020-07-19 11:07:37 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -9,25 +9,27 @@ git config --global user.email "balandiomer@gmail.com"
 git config --global user.name "obsfx"
 
 sudo apt install python3-pip
-sudo apt install neovim
 sudo apt install zsh
 
 sudo apt install clangd-9
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-9 100
-
-sudo pip3 install pynvim
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh) $(whoami)
 
-mkdir ~/.config/nvim -p
+mkdir ~/.config/gtk-3.0 -p
+curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/gtk-3.0/gtk.css" > ~/.config/gtk-3.0/gtk.css
 
-curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/init.vim" > ~/.config/nvim/init.vim
-
+sudo apt install neovim
 mkdir ~/.vim/undodir -p
+mkdir ~/.config/nvim -p
+curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/nvim/init.vim" > ~/.config/nvim/init.vim
 
-echo ':PlugInstall'
+sudo apt install alacritty
+mkdir ~/.config/alacritty -p
+curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/alacritty/alacritty.yml" > ~/.config/alacritty/alacritty.yml
 
-echo ':CocInstall coc-css coc-json coc-html coc-tsserver coc-clangd coc-highlight coc-markdownlint coc-python coc-git|q'
+sudo apt install tmux
+curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/.tmux.conf" > ~/.tmux.conf
