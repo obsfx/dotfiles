07ea8d846edc962d256f484b0cb613245d61fb5f
Ömercan Balandı <balandiomer@gmail.com>
2021-01-14 19:44:53 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/gtk.css" > ~/.conf
 # sudo apt install libxft-dev
 # sudo apt install ttf-ancient-fonts-symbola
 
-kitty
+# kitty
 sudo apt install kitty
 mkdir ~/.config/kitty
 curl "https://raw.githubusercontent.com/obsfx/dotfiles/master/kitty.conf" > ~/.config/kitty/kitty.conf
