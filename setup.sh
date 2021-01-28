bb37f5d7ff1be6a0cb04fb7111e6639653b75c8a
Ömercan Balandı <balandiomer@gmail.com>
2021-01-28 06:07:54 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -24,14 +24,14 @@ sudo apt install tmux
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/obsfx/dotfiles/master/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# gtk 
+# gtk
 curl -o ~/.config/gtk-3.0/gtk.css https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/gtk-3.0/gtk.css
 
-# st.desktop 
+# st.desktop
 sudo curl -o /usr/share/applications/st.desktop https://raw.githubusercontent.com/obsfx/dotfiles/master/usr/share/applications/st.desktop
 
-# libs for st
-# packages for compiling simple terminal
+# packages to compiling simple terminal
+# sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/st 40
 sudo apt install libx11-dev
 sudo apt install libxft-dev
 sudo apt install ttf-ancient-fonts-symbola
@@ -71,7 +71,7 @@ curl -o ~/.zshrc https://raw.githubusercontent.com/obsfx/dotfiles/master/.zshrc
 
 # regolith
 sudo apt install i3xrocks-focused-window-name
-sudo apt install regolith-desktop-standard 
+sudo apt install regolith-desktop-standard
 curl -o ~/.config/regolith/Xresources https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/regolith/Xresources
 curl -o ~/.rofi https://raw.githubusercontent.com/obsfx/dotfiles/master/.rofi
 curl -o ~/.wallpapers/bg.jpg --create-dirs https://raw.githubusercontent.com/obsfx/dotfiles/master/.wallpapers/bg.jpg
