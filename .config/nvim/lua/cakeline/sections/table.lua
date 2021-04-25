fd241fa29958d542d1d6788e454e19d10cbe7fcc
Ömercan Balandı <balandiomer@gmail.com>
2021-04-25 18:11:48 +0300

    enough 🍰 for me

--- .config/nvim/lua/cakeline/sections/table.lua
+++ .config/nvim/lua/cakeline/sections/table.lua
@@ -1,8 +1,8 @@
 local table = {
   _sl = {
     config = {
-      activecolors = {bg = '#222222', fg = '#777777', gui = 'NONE'},
-      inactivecolors = {bg = '#1c1b1a', fg = '#444444', gui = 'NONE'}
+      activecolors = {bg = '#262626', fg = '#888888', gui = 'NONE'},
+      inactivecolors = {bg = '#1d1d1d', fg = '#444444', gui = 'NONE'}
     }
   },
   switchright = {
@@ -16,24 +16,24 @@ local table = {
       rightpadding = 1,
       colormap = {
         ['n'] = {bg = '#282828', fg = '#eeeeee', gui = 'BOLD'},
-        ['no'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
+        ['no'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
         ['v'] = {bg = '#E1ECF3', fg = '#282828', gui = 'BOLD'},
         ['V'] = {bg = '#E1ECF3', fg = '#282828', gui = 'BOLD'},
         [''] = {bg = '#E1ECF3', fg = '#282828', gui =  'BOLD'},
-        ['s'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['S'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        [''] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['i'] = {bg = '#1CACF4', fg = '#282828', gui = 'BOLD'},
-        ['R'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
+        ['s'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['S'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        [''] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['i'] = {bg = '#E1ECF3', fg = '#282828', gui = 'BOLD'},
+        ['R'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
         ['Rv'] = {bg = '#E1ECF3', fg = '#282828', gui = 'BOLD'},
-        ['c'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['cv'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['ce'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['r'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['rm'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['r?'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['!'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
-        ['t'] = {bg = '#CF2549', fg = '#eeeeee', gui = 'BOLD'},
+        ['c'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['cv'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['ce'] = {bg = '#F4DC8C', fg = '#28', gui = 'BOLD'},
+        ['r'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['rm'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['r?'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['!'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
+        ['t'] = {bg = '#F4DC8C', fg = '#282828', gui = 'BOLD'},
         ['inactive'] = {bg = '#1c1b1a', fg = '#444444', gui = 'BOLD'}
       }
     },
@@ -45,7 +45,7 @@ local table = {
       leftpadding = 1,
       rightpadding = 0,
       activecolors = {bg = '#222222', fg = '#777777', gui = 'NONE'},
-      inactivecolors = {bg = '#1c1b1a', fg = '#444444', gui = 'NONE'}
+      inactivecolors = {bg = '#1d1d1d', fg = '#444444', gui = 'NONE'}
     },
     show = require('cakeline.sections.filename').show
   }
