1c7c887cfb0364b41d3a9a8e1439bc805902b00e
obsfx <balandiomer@gmail.com>
2021-06-06 02:40:08 +0300

    regolith

---
+++ regolith.sh
@@ -0,0 +1,7 @@
+# regolith
+# sudo apt install i3xrocks-focused-window-name
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt install regolith-desktop-standard
+curl -o ~/.config/regolith/Xresources https://raw.githubusercontent.com/obsfx/dotfiles/master/.config/regolith/Xresources
+curl -o ~/.rofi https://raw.githubusercontent.com/obsfx/dotfiles/master/.rofi
+curl -o ~/.wallpapers/bg.jpg --create-dirs https://raw.githubusercontent.com/obsfx/dotfiles/master/.wallpapers/bg.jpg
